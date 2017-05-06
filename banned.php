acab3f64ae0a73c010765bec7dbf799d96885703
dragonzx <DragonZX@yandex.ru>
2017-05-06 13:08:46 +0300

    Fix missing

--- banned.php
+++ banned.php
@@ -8,8 +8,8 @@ $result = $connection->query($query);
 <html>
 <head>
 <title>Banned users</title>
-<link href="/css/bootstrap.css" rel="stylesheet" type="text/css" />
-    <link href="/css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -18,9 +18,11 @@ $result = $connection->query($query);
 
 <body>
 <div id="wrapper">
-<label for="textfield">Banned user:</label>
-<input type="text" name="textfield" id="textfield">
-<button type="button" class="btn btn-primary">Filter</button>
+    <form method="get">
+        <label for="textfield">Banned user:</label>
+        <input type="text" name="textfield" id="textfield">
+<button type="submit" class="btn btn-primary">Filter</button>
+    </form>
     <h1>Banned users</h1>
 <table class="table table-striped table-bordered">
     <caption>Banned users</caption>
